9699ad1b20c5e13dba1d396163ce4e63cfb535f4
kimtaes <kimtaes@oregonstate.edu>
2021-03-08 10:19:58 +0900

    Second test
    Added two test cases: 2012 and 1900. It should return yes and no respectively.

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,5 +6,12 @@ class TestCase(unittest.TestCase):
         # 1997: a year not divisible by 4, 100 or 400
         self.assertEqual(leapyear.IsLeapYear(1997), 'NO')
 
+        # 2012: divisible by 4 but not by 100 and 400
+        self.assertEqual(leapyear.IsLeapYear(2012), 'YES')
+
+        #1900: divisible by 4 and 100 but not by 400
+        self.assertEqual(leapyear.IsLeapYear(1900), 'NO')
+
+        
 if __name__ == '__main__':
     unittest.main()

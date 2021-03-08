e36fd749d7101a607b673b3f4b485670af81272a
kimtaes <kimtaes@oregonstate.edu>
2021-03-08 10:27:24 +0900

    final update
    Now the function is able to determine years like 2000.

--- leapyear.py
+++ leapyear.py
@@ -1,8 +1,11 @@
 def IsLeapYear(year):
     if(year % 4) == 0:
         if( year % 100) == 0:
-            return 'NO'
-        
+            if(year % 400) == 0:
+                return 'YES'
+            else:
+                return 'NO'
+                
         return 'YES'
 
     else:
